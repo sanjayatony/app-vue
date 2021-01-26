c7d7bb8c80055068b85e9a14e0fd6561c0e93b39
Tony S <sanjayatony@gmail.com>
2021-01-26 17:02:33 +0700

    req to api

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,12 +5,14 @@
       <div class="rounded-md shadow-sm -space-y-px">
         <input
           type="text"
+          v-model="username"
           required
           class="appearance-none relative rounded-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10"
           placeholder="Username"
         />
         <input
           type="password"
+          v-model="passwd"
           required
           autocomplete="current-password"
           placeholder="Password"
@@ -24,6 +26,7 @@
       </div>
       <div>
         <button
+          @click="login"
           type="submit"
           class="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
         >
@@ -33,3 +36,24 @@
     </form>
   </div>
 </template>
+<script>
+import axios from "axios";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://dash.test";
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      passwd: "",
+    };
+  },
+  methods: {
+    login() {
+      axios.get("/sanctum/csrf-cookie").then((response) => {
+        console.log(response);
+      });
+    },
+  },
+};
+</script>
