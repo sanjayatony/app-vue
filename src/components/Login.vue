7a78a15ad99d17a2e16ab749017885c69e395aa2
Tony S <sanjayatony@gmail.com>
2021-01-26 16:27:43 +0700

    form login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1 class="uppercase">Hello World</h1>
+  <div class="max-w-md mx-auto mt-16">
+    <h1 class="text-center text-3xl font-bold">Admin Page</h1>
+    <form class="p-4">
+      <div class="rounded-md shadow-sm -space-y-px">
+        <input
+          type="text"
+          required
+          class="appearance-none relative rounded-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10"
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          required
+          autocomplete="current-password"
+          placeholder="Password"
+          class="appearance-none relative rounded-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10"
+        />
+      </div>
+      <div class="text-sm py-2">
+        <a href="#" class="text-purple-600 hover:text-purple-500">
+          Forgot your password?
+        </a>
+      </div>
+      <div>
+        <button
+          type="submit"
+          class="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
