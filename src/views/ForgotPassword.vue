6e471c57b55c4c3c7125d366561246c61179bcc6
Tony S <sanjayatony@gmail.com>
2021-01-27 14:27:15 +0700

    forgot passwd page

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <h1 class="text-center text-3xl font-bold mb-4">Forgot Password</h1>
+    <div v-if="errorMessage" class="text-red-500 mb-4 text-sm">
+      {{ errorMessage }}
+    </div>
+    <form action="#" method="POST" @submit.prevent="forgotPassword">
+      <div class="rounded-md shadow-sm mb-4">
+        <input
+          type="email"
+          v-model="email"
+          required
+          class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+          class="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+        >
+          Send
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://dash.test";
+export default {
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    forgotPassword() {},
+    login() {
+      axios.get("/sanctum/csrf-cookie").then(() => {
+        axios
+          .post("/login", {
+            email: this.email,
+            password: this.password,
+          })
+          .then(() => {
+            localStorage.setItem("isLoggedIn", "true");
+            this.$router.push({ name: "Dashboard" });
+          })
+          .catch((error) => {
+            const key = Object.keys(error.response.data.errors)[0];
+            this.errorMessage = error.response.data.errors[key][0];
+          });
+      });
+    },
+  },
+};
+</script>
