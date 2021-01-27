8ee42208e68b6d94527ff9486903a5cde8ad8884
Tony S <sanjayatony@gmail.com>
2021-01-27 15:43:47 +0700

    add error message and counter

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home">
     <h1 class="text-center text-3xl font-bold mb-4">Admin Page</h1>
-    <div v-if="errorMessage" class="text-red-500 mb-4 text-sm">
+    <div v-if="blocked" class="text-gray-900 mb-4 text-center">
+      <div class="text-red-600 font-bold text-sm">Too much login attempts, please wait.</div>
+      <div class="text-gray-900 text-xl font-bold">{{ timerCount }}</div>
+    </div>
+    <div v-if="errorMessage && !blocked" class="text-red-500 mb-4 text-sm">
       {{ errorMessage }}
     </div>
-    <form action="#" method="POST" @submit.prevent="login">
+    <form v-if="!blocked" action="#" method="POST" @submit.prevent="login">
       <div class="rounded-md shadow-sm -space-y-px">
         <input
           type="email"
@@ -51,6 +55,9 @@ export default {
       email: "",
       password: "",
       errorMessage: "",
+      counter: 0,
+      timerCount: 0,
+      blocked: false,
     };
   },
   methods: {
@@ -66,10 +73,27 @@ export default {
             this.$router.push({ name: "Dashboard" });
           })
           .catch((error) => {
+            this.counter += 1;
+            if (this.counter === 3) {
+              this.timerCount = 600;
+              this.blocked = true;
+            }
             const key = Object.keys(error.response.data.errors)[0];
             this.errorMessage = error.response.data.errors[key][0];
           });
       });
+    },
+  },
+  watch: {
+    timerCount: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.timerCount--;
+          }, 1000);
+        }
+      },
+      immediate: true,
     },
   },
 };
