5995b71ba4ef622a78fcbd645dd563a040eb71d6
Tony S <sanjayatony@gmail.com>
2021-01-27 14:26:57 +0700

    add forgot password

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/Dashboard.vue"),
   },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/ForgotPassword.vue"),
+  },
 ];
 
 const router = new VueRouter({
