201bbe2e85b4231dd60898fb51dfee492645c094
Tony S <sanjayatony@gmail.com>
2021-01-27 13:54:16 +0700

    remove padding

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="bg-gray-100">
+  <div id="app" class="bg-gray-100 pt-16">
     <div class="max-w-xl mx-auto px-4 py-16 border border-gray-300 rounded-md bg-white">
       <router-view />
     </div>
